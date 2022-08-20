be30b75dbf45333229d2678814cf79f88a01e6f1
Joshimello <joshualeanjw@gmail.com>
2022-08-20 22:29:49 +0800

    main page frontend + file upload system

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,25 @@ const socket = io()
 
 $('#main').load('html/login.html')
 
-$('#login').submit((e) => {
+$(document).on('submit', '#loginform', e => {
     e.preventDefault()
-    socket.emit('login', {"username": $('#username').val(), "password": $('#password').val()}, (err, msg) => {
+    socket.emit('login', { "username": $('#username').val(), "password": $('#password').val() }, (err, msg) => {
         msg == 'username' ? $('#username').addClass('is-invalid') : $('#username').removeClass('is-invalid')
         msg == 'password' ? $('#password').addClass('is-invalid') : $('#password').removeClass('is-invalid')
     })
 })
 
 socket.on('join', value => {
-
+    console.log(value)
+    $('#main').load('html/main.html', () => {
+        $('#loginname').text(value.toUpperCase())
+    })
+})
+    
+$('#main').filedrop({
+    callback: data => {
+        socket.emit('upload', data, (status) => {
+            console.log(status)
+        })
+    }
 })
